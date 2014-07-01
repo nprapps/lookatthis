99d0cc04e32748c892c82e499370102a4af7ff54
Christopher Groskopf <staringmonkey@gmail.com>
2014-07-01 12:55:40 -0500

    Initial import from app-template.

---
+++ app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import json
+
+import argparse
+from flask import Flask, render_template
+
+import app_config
+from render_utils import make_context, smarty_filter, urlencode_filter
+import static
+
+app = Flask(__name__)
+
+app.jinja_env.filters['smarty'] = smarty_filter
+app.jinja_env.filters['urlencode'] = urlencode_filter
+
+# Example application views
+@app.route('/')
+def index():
+    """
+    Example view demonstrating rendering a simple HTML page.
+    """
+    context = make_context()
+
+    with open('data/featured.json') as f:
+        context['featured'] = json.load(f)
+
+    return render_template('index.html', **context)
+
+@app.route('/comments/')
+def comments():
+    """
+    Full-page comments view.
+    """
+    return render_template('comments.html', **make_context())
+
+@app.route('/widget.html')
+def widget():
+    """
+    Embeddable widget example page.
+    """
+    return render_template('widget.html', **make_context())
+
+@app.route('/test_widget.html')
+def test_widget():
+    """
+    Example page displaying widget at different embed sizes.
+    """
+    return render_template('test_widget.html', **make_context())
+
+app.register_blueprint(static.static)
+
+# Boilerplate
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port')
+    args = parser.parse_args()
+    server_port = 8000
+
+    if args.port:
+        server_port = int(args.port)
+
+    app.run(host='0.0.0.0', port=server_port, debug=app_config.DEBUG)
