1a0e1fe1a7673f7ca011f22d991a5234b06b92df
Tyler Fisher <tyler@tylerjfisher.com>
2014-07-01 16:25:26 -0400

    basic new_post template

---
+++ new_post/www/js/app.js
@@ -0,0 +1,83 @@
+// Global jQuery references
+var $shareModal = null;
+var $commentCount = null;
+
+// Global state
+var firstShareLoad = true;
+
+/*
+ * Run on page load.
+ */
+var onDocumentLoad = function(e) {
+    // Cache jQuery references
+    $shareModal = $('#share-modal');
+    $commentCount = $('.comment-count');
+
+    // Bind events
+    $shareModal.on('shown.bs.modal', onShareModalShown);
+    $shareModal.on('hidden.bs.modal', onShareModalHidden);
+
+    renderExampleTemplate();
+    getCommentCount(showCommentCount);
+}
+
+/*
+ * Basic templating example.
+ */
+var renderExampleTemplate = function() {
+    var context = $.extend(APP_CONFIG, {
+        'template_path': 'jst/example.html',
+        'config': JSON.stringify(APP_CONFIG, null, 4),
+        'copy': JSON.stringify(COPY, null, 4)
+    });
+
+    var html = JST.example(context);
+
+    $('#template-example').html(html);
+}
+
+/*
+ * Display the comment count.
+ */
+var showCommentCount = function(count) {
+    $commentCount.text(count);
+
+    if (count > 0) {
+        $commentCount.addClass('has-comments');
+    }
+
+    if (count > 1) {
+        $commentCount.next('.comment-label').text('Comments');
+    }
+}
+
+/*
+ * Share modal opened.
+ */
+var onShareModalShown = function(e) {
+    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'open-share-discuss']);
+
+    if (firstShareLoad) {
+        loadComments();
+
+        firstShareLoad = false;
+    }
+}
+
+/*
+ * Share modal closed.
+ */
+var onShareModalHidden = function(e) {
+    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'close-share-discuss']);
+}
+
+/*
+ * Text copied to clipboard.
+ */
+var onClippyCopy = function(e) {
+    alert('Copied to your clipboard!');
+
+    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'summary-copied']);
+}
+
+$(onDocumentLoad);
