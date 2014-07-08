5646e27809fd61e3aaa4ae72d397f8a89994254e
Tyler Fisher <tyler@tylerjfisher.com>
2014-07-08 09:59:54 -0400

    fix _theme_static

--- theme.py
+++ theme.py
@@ -10,12 +10,13 @@ import copytext
 import envoy
 from flask import Blueprint
 from render_utils import flatten_app_config, flatten_post_config
+import static
 
 theme = Blueprint('theme', __name__, url_prefix='/tumblr')
 
 # Render LESS files on-demand
 @theme.route('/less/<string:filename>')
-def _theme_less(slug, filename):
+def _theme_less(filename):
 
     return static.less('tumblr', filename)
 
@@ -29,10 +30,10 @@ def _app_config_js():
 
 # render copytext
 @theme.route('/js/copy.js')
-def _copy_js(slug):
+def _copy_js():
     return static.copy_js('theme')
 
 # serve arbitrary static files on-demand
 @theme.route('/<path:path>')
-def _theme_static(slug, path):
+def _theme_static(path):
     return static.static_file('tumblr', path)
