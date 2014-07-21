f76d7a6ce9b34c1827a34c18f53acef08ed3563f
lindamood <wesley.lindamood@gmail.com>
2014-07-21 12:58:08 -0400

    removing post

--- posts/2014-07-16-pink/www/js/post_config.js
+++
@@ -1,1 +0,0 @@
-window.POST_CONFIG = {"GOOGLE_ANALYTICS": {"TOPICS": "", "DOMAIN": "apps.npr.org", "ACCOUNT_ID": "UA-5828686-4"}, "IS_PUBLISHED": {"development": true, "production": false, "staging": false}, "TARGET_IDS": {"development": "91880057552", "production": null, "staging": null}, "COPY_GOOGLE_DOC_KEY": "0AqjLQISCZzBkdGhkNmZsMmw3Ry1uZnY5M3FwVXlnd3c"}
