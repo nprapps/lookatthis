163ca2658c8c1e3cc858c319029f0bb033585972
lindamood <wesley.lindamood@gmail.com>
2014-07-24 15:07:05 -0400

    adding draft hello world post

---
+++ posts/hello/post_config.py
@@ -0,0 +1,25 @@
+COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AsbgU6Ugtx7udGZwclVEbktmYTIyb2dZM3U3M0dWSXc&usp=drive_web#gid=1'
+GOOGLE_ANALYTICS = {
+    'ACCOUNT_ID': 'UA-5828686-4',
+    'DOMAIN': 'apps.npr.org',
+    'TOPICS': '' # e.g. '[1014,3,1003,1002,1001]'
+}
+SLUG = 'hello'
+
+
+
+"""
+DO NOT EDIT THESE
+
+DO. NOT. EDIT. THESE.
+"""
+TARGET_IDS = {
+    'development': '92749320807',
+    'staging': None,
+    'production': None,
+}
+IS_PUBLISHED = {
+    'development': True,
+    'staging': False,
+    'production': False
+}
