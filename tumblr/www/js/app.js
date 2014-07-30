d52bafd4a1cc0f9ad044e9043893b43a285f41a4
Christopher Groskopf <staringmonkey@gmail.com>
2014-07-30 08:38:18 -0500

    Clean up tumblr app.js.

--- tumblr/www/js/app.js
+++ tumblr/www/js/app.js
@@ -1,90 +1,15 @@
 // Global jQuery references
-var $shareModal = null;
-var $commentCount = null;
 var $firstPost = null;
-
-// Global state
-var firstShareLoad = true;
 
 /*
  * Run on page load.
  */
 var onDocumentLoad = function(e) {
-    // Cache jQuery references
-    // $shareModal = $('#share-modal');
-    // $commentCount = $('.comment-count');
     $firstPost = $('#posts').find('.post').first();
 
     var $firstPostImage = $firstPost.find('img');
 
     $firstPostImage.attr('src', $firstPostImage.data('highres-image'));
-
-    // Bind events
-    // $shareModal.on('shown.bs.modal', onShareModalShown);
-    // $shareModal.on('hidden.bs.modal', onShareModalHidden);
-
-    // renderExampleTemplate();
-    // getCommentCount(showCommentCount);
-
-}
-
-/*
- * Basic templating example.
- */
-// var renderExampleTemplate = function() {
-//     var context = $.extend(APP_CONFIG, {
-//         'template_path': 'jst/example.html',
-//         'config': JSON.stringify(APP_CONFIG, null, 4),
-//         'copy': JSON.stringify(COPY, null, 4)
-//     });
-
-//     var html = JST.example(context);
-
-//     $('#template-example').html(html);
-// }
-
-/*
- * Display the comment count.
- */
-var showCommentCount = function(count) {
-    $commentCount.text(count);
-
-    if (count > 0) {
-        $commentCount.addClass('has-comments');
-    }
-
-    if (count > 1) {
-        $commentCount.next('.comment-label').text('Comments');
-    }
-}
-
-/*
- * Share modal opened.
- */
-var onShareModalShown = function(e) {
-    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'open-share-discuss']);
-
-    if (firstShareLoad) {
-        loadComments();
-
-        firstShareLoad = false;
-    }
-}
-
-/*
- * Share modal closed.
- */
-var onShareModalHidden = function(e) {
-    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'close-share-discuss']);
-}
-
-/*
- * Text copied to clipboard.
- */
-var onClippyCopy = function(e) {
-    alert('Copied to your clipboard!');
-
-    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'summary-copied']);
 }
 
 $(onDocumentLoad);
