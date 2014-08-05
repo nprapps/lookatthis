d62b5869f07a00d205d98914d3479ee0b5bef990
Christopher Groskopf <staringmonkey@gmail.com>
2014-08-05 10:20:08 -0500

    Analytics config does not belong in post_config.

--- posts/hello/post_config.py
+++ posts/hello/post_config.py
@@ -1,9 +1,4 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AsbgU6Ugtx7udGZwclVEbktmYTIyb2dZM3U3M0dWSXc&usp=drive_web#gid=1'
-GOOGLE_ANALYTICS = {
-    'ACCOUNT_ID': 'UA-5828686-4',
-    'DOMAIN': 'apps.npr.org',
-    'TOPICS': '' # e.g. '[1014,3,1003,1002,1001]'
-}
 SLUG = 'hello'
 
 
--- posts/public-housing/post_config.py
+++ posts/public-housing/post_config.py
@@ -1,9 +1,4 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AsbgU6Ugtx7udEEtenZsZ2xBVlJXbzdPTEJJSGxWN0E&usp=drive_web#gid=9'
-GOOGLE_ANALYTICS = {
-    'ACCOUNT_ID': 'UA-5828686-4',
-    'DOMAIN': 'apps.npr.org',
-    'TOPICS': '' # e.g. '[1014,3,1003,1002,1001]'
-}
 
 
 """
