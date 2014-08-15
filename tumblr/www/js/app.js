805b280fae5e07143e4bce0d46d29cd2bc5210fb
Danny DeBelius <danny.debelius@gmail.com>
2014-08-15 15:31:33 -0400

    Post styles, fix infinite scroll

--- tumblr/www/js/app.js
+++ tumblr/www/js/app.js
@@ -1,15 +1,58 @@
-// Global jQuery references
-var $firstPost = null;
+var $window;
+var $document;
+var $videoContainers;
 
-/*
- * Run on page load.
- */
-var onDocumentLoad = function(e) {
-    $firstPost = $('#posts').find('.post').first();
+var sizeVideoContainers = function(element){
+	var scope = element||document;
+	var $videoIframe = $(scope).find('.tumblr_video_iframe, .spotify_audio_player, .video-container img');
 
-    var $firstPostImage = $firstPost.find('img');
+	$videoIframe.each(function(){
+		var $this = $(this);
 
-    $firstPostImage.attr('src', $firstPostImage.data('highres-image'));
+		// Don't recalculate if we've already assigned an aspect ratio
+		if ($this.parents('.video-container').hasClass('nine-by-sixteen vertical square sixteen-by-nine')){
+			return;
+		}
+
+		var height = parseInt($this.attr('height'));
+		var width = parseInt($this.attr('width'));
+
+		if (height == 780 && width == 700) {
+			$this.attr('height', '100%');
+			$this.attr('width', '100%');
+			$this.parents('.video-container').addClass('spotify');
+		}
+
+		if (height > width){
+			$this.parents('.video-container').addClass('nine-by-sixteen');
+			$this.parents('.video-wrapper').addClass('vertical');
+		} else if (height == width) {
+			$this.parents('.video-container').addClass('square');
+		} else {
+			$this.parents('.video-container').addClass('sixteen-by-nine');
+		}
+
+		$this.parent().attr('style', '');
+	});
 }
 
-$(onDocumentLoad);
+$(function() {
+	$window = $(window);
+	$document = $(document);
+	$videoContainers = $('.video-container');
+
+
+
+	$videoContainers.fitVids({ customSelector: "video"});
+
+	$window.on('resize', function(){
+		$vineEmbeds = $document.find('iframe.vine-embed');
+		$vineEmbeds.each(function(){
+			var vidSrc = $(this).attr('src');
+			$(this).attr('src', vidSrc);
+		});
+	});
+
+	sizeVideoContainers();
+
+});
