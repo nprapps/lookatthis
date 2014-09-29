bd104d3a9d7c73d651e0e69c1d973f5d10c5edd9
Christopher Groskopf <staringmonkey@gmail.com>
2014-09-29 16:14:50 -0500

    Handle line breaks. Strip italics.

--- posts/colors/generate_quote_images.py
+++ posts/colors/generate_quote_images.py
@@ -23,14 +23,14 @@ OUT_DIR = 'www/share-images'
 
 CANVAS_WIDTH = 640
 CANVAS_HEIGHT = 640
-BODY_MARGIN = Margin(top=200, right=40, bottom=200, left=150)
+BODY_MARGIN = Margin(top=200, right=40, bottom=75, left=150)
 TEXT_MAX_WIDTH = CANVAS_WIDTH - (BODY_MARGIN.left + BODY_MARGIN.right)
 
-SIZE_MIN = 16 
-SIZE_MAX = 64
-SIZE_DELTA = 4
+SIZE_MIN = 12
+SIZE_MAX = 24 
+SIZE_DELTA = 2 
 
-LINE_MIN = 20
+LINE_MIN = 16 
 LINE_MAX = 50
 LINE_DELTA = 2 
 
@@ -48,8 +48,12 @@ fonts['normal'] = {}
 fonts['bold'] = {}
 
 def strip_tags(text):
-    text = text.replace(u'<p>', u'')
-    text = text.replace('</p>', '\n\n')
+    text = text.replace('<p>', '')
+    text = text.replace('</p>', '')
+    text = text.replace('<i>', '')
+    text = text.replace('</i>', '')
+    text = text.replace('<em>', '')
+    text = text.replace('</em>', '')
     text = text.replace('&#34;', '"')
     text = text.replace('&quot;', '"')
     text = text.replace('&mdash;', u'—')
@@ -74,9 +78,9 @@ def compute_size(lines, fontsize):
 def optimize_text(text, max_height):
     permutations = {}
     
-    for size in fonts['bold'].keys():
+    for size in range(16, 24, 2):
         for wrap_count in xrange(LINE_MIN, LINE_MAX + 1, LINE_DELTA):
-            lines = textwrap.wrap(text, wrap_count)
+            lines = textwrap.wrap(text, wrap_count, replace_whitespace=False, drop_whitespace=False)
 
             width, height = compute_size(lines, size)
             height += height / len(lines) * 3
@@ -128,36 +132,54 @@ def render(slug, icon_filename, title, body, source):
 
         icon = ICONS[icon_filename]
 
-        img.paste(icon, (40, BODY_MARGIN.top - 40), mask=icon)
+        img.paste(icon, (40, BODY_MARGIN.top - 50), mask=icon)
 
     # Title
     text = title.upper()
     font = fonts['bold'][24]
     width = font.getsize(text)[0]
 
-    draw.text((BODY_MARGIN.left, BODY_MARGIN.top - 40), text, font=font, fill=(255, 255, 255))
-    draw.line([(BODY_MARGIN.left, BODY_MARGIN.top - 16), (BODY_MARGIN.left + width, BODY_MARGIN.top - 16)], width=4, fill=(255, 255, 255))
+    draw.text((BODY_MARGIN.left, BODY_MARGIN.top - 50), text, font=font, fill=(255, 255, 255))
+    draw.line([(BODY_MARGIN.left, BODY_MARGIN.top - 24), (BODY_MARGIN.left + width, BODY_MARGIN.top - 24)], width=4, fill=(255, 255, 255))
 
     # Body
     text = strip_tags(unicode(body))
+    text = text.replace('\n\n', 'SPLITHERE')
 
     max_height = CANVAS_HEIGHT - (BODY_MARGIN.top + BODY_MARGIN.bottom) 
     size, wrap_count = optimize_text(text, max_height)
     lines = textwrap.wrap(text, wrap_count)
 
+    out_lines = []
+
+    for line in lines:
+        if 'SPLITHERE' in line:
+            parts = line.split('SPLITHERE')
+            out_lines.extend([parts[0], '', parts[1]])
+        else:
+            out_lines.append(line)
+
     y = BODY_MARGIN.top
 
-    for i, line in enumerate(lines):
-
+    for i, line in enumerate(out_lines):
         draw.text((BODY_MARGIN.left, y), line, font=fonts['normal'][size], fill=(255, 255, 255))
 
-        y += size * 1.15
+        y += size * 1.25
+
+    # Source
+    if source:
+        text = 'Source: %s' % source
+        font = fonts['bold'][12]
+        width = font.getsize(text)[0]
+        draw.text((CANVAS_WIDTH - (15 + width), 585), text, font=font, fill=(255, 255, 255))
 
     # Footer
     img.paste(FOOTER, (0, CANVAS_HEIGHT - 35))
 
+    text = 'http://n.pr/colors'
     font = fonts['bold'][16]
-    draw.text((490, 618), 'http://n.pr/colors', font=font, fill=(255, 255, 255))
+    width = font.getsize(text)[0]
+    draw.text((CANVAS_WIDTH - (15 + width), 618), text, font=font, fill=(255, 255, 255))
 
     img.save('%s/%s.png' % (OUT_DIR, slug), 'PNG')
 
