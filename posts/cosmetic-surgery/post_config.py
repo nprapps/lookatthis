18491a5525866940edaac3ddc39cfc1c72cd366b
Claire O'Neill <coneill@npr.org>
2014-10-08 17:30:48 -0400

    edited slug

--- posts/cosmetic-surgery/post_config.py
+++ posts/cosmetic-surgery/post_config.py
@@ -1,2 +1,2 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AiIfOsKv5mKldDVhakpwdXhUZGloUzZfV1VlRnVtWEE&usp=drive_web#gid=1'
-DEPLOY_SLUG = 'cosmetic-surgery'
+DEPLOY_SLUG = 'plastic'
