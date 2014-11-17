903fdfdcef1f7960669c055e2417ce6a0b796348
Claire O'Neill <coneill@npr.org>
2014-11-17 16:40:32 -0500

    share language

--- posts/dennis-stock/post_config.py
+++ posts/dennis-stock/post_config.py
@@ -1,2 +1,2 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AiIfOsKv5mKldEhXSGpUbzltd1NkZ2lYVnVKZHpQcVE&usp=sharing#gid=1'
-DEPLOY_SLUG = 'dennis-stock'
+DEPLOY_SLUG = 'erinm'
