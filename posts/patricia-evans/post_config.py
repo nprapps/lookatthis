7556566781883f449dcd009a8bdb58c721677e2c
lindamood <wesley.lindamood@gmail.com>
2014-12-11 12:10:45 -0500

    roughing out the full bleed and side by side templates

--- posts/patricia-evans/post_config.py
+++ posts/patricia-evans/post_config.py
@@ -1,2 +1,7 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0Ak3IIavLYTovdEp5YlF3d3dic1Jaa0VKbk8zR1p3Wmc'
 DEPLOY_SLUG = 'patricia-evans-alt'
+
+
+#
+
+#https://docs.google.com/spreadsheet/ccc?key=0AsbgU6Ugtx7udDI2cU1Jd0U4SDhsNWw4QVFTWGo2Qmc
