a45b6bf03419aa0b432765601b38aa097e2d5184
lindamood <wesley.lindamood@gmail.com>
2014-12-13 22:00:54 -0500

    styles for single image template and tweaks to side-by-side

--- posts/patricia-evans/post_config.py
+++ posts/patricia-evans/post_config.py
@@ -1,7 +1,7 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0Ak3IIavLYTovdEp5YlF3d3dic1Jaa0VKbk8zR1p3Wmc'
 DEPLOY_SLUG = 'patricia-evans'
 
-#shared copy
+#shared copy 
 
 #local copy https://docs.google.com/spreadsheet/ccc?key=0AsbgU6Ugtx7udDI2cU1Jd0U4SDhsNWw4QVFTWGo2Qmc
 
