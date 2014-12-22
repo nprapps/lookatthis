f92ea798e89a65734b5de8ad9c31c20b73938a15
lindamood <wesley.lindamood@gmail.com>
2014-12-22 10:15:43 -0500

    adjusting paddles and width of content well on small screen

--- posts/patricia-evans/www/js/app.js
+++ posts/patricia-evans/www/js/app.js
@@ -336,22 +336,6 @@ var trackEvent = function(args) {
     _gaq.push(args)
 }
 
-var fakeMobileHover = function() {
-    $(this).css({
-        'background-color': '#fff',
-        'color': '#000',
-        'opacity': .9
-    });
-}
-
-var rmFakeMobileHover = function() {
-    $(this).css({
-        'background-color': 'rgba(0, 0, 0, 0.2)',
-        'color': '#fff',
-        'opacity': .3
-    });
-}
-
 $(document).ready(function() {
     $w = $(window).width();
     $h = $(window).height();
@@ -374,9 +358,6 @@ $(document).ready(function() {
     //$startCardButton.on('click', onStartCardButtonClick);
     $slides.on('click', onSlideClick);
     $upNext.on('click', onNextPostClick);
-
-    $arrows.on('touchstart', fakeMobileHover);
-    $arrows.on('touchend', rmFakeMobileHover);
 
     
 
