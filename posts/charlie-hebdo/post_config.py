0afd8107c43f199d09b105f67c0d9094277d0ad8
Tyler Fisher <tyler@tylerjfisher.com>
2015-01-07 11:47:55 -0500

    fix url to spreadsheet, port side-by-side and single-image templates from cities

--- posts/charlie-hebdo/post_config.py
+++ posts/charlie-hebdo/post_config.py
@@ -1,2 +1,2 @@
-COPY_GOOGLE_DOC_URL = 'https://docs.google.com/a/tylerjfisher.com/spreadsheet/ccc?key=0AqjLQISCZzBkdFkzV0trMVFCV043dXBWSWtsQkp4OGc#gid=9'
+COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AqjLQISCZzBkdFkzV0trMVFCV043dXBWSWtsQkp4OGc'
 DEPLOY_SLUG = 'charlie-hebdo'
