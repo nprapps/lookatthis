80aadd6c8c3df4feb4a4a99b5caa5d400c5e0463
Tyler Fisher <tyler@tylerjfisher.com>
2015-01-12 11:13:44 -0500

    port over default app template analytics. closes #632

--- new_post/post_config.py
+++ new_post/post_config.py
@@ -1,2 +1,4 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AqjLQISCZzBkdGdxRXdtVDNDMzIwNmN3S2RQd196NUE&usp=drive_web#gid=9'
 DEPLOY_SLUG = ''
+NUM_SLIDES_AFTER_CONTENT = 1
+GOOGLE_ANALYTICS_TOPICS = []
