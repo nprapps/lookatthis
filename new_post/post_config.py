573fdf9a641fb36f4def5a84d6db517f21c9c9c8
David Eads <davideads@gmail.com>
2015-01-13 16:58:29 -0500

    copy over latest changes to new_post

--- new_post/post_config.py
+++ new_post/post_config.py
@@ -1,3 +1,3 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AqjLQISCZzBkdGdxRXdtVDNDMzIwNmN3S2RQd196NUE&usp=drive_web#gid=9'
-DEPLOY_SLUG = 'test-project'
+DEPLOY_SLUG = ''
 NUM_SLIDES_AFTER_CONTENT = 2
