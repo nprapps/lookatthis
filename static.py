739921e6baf07538376bb44886f10933d24fb9e4
Tyler Fisher <tyler@tylerjfisher.com>
2015-02-06 17:01:56 -0500

    make it work in chrome

--- static.py
+++ static.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 from mimetypes import guess_type
+import os
+from re import findall
 import subprocess
 
 from flask import abort
+from werkzeug.datastructures import Headers
 
 import copytext
 from flask import Blueprint
@@ -22,6 +25,44 @@ def copy_js(slug):
     copy = 'window.copy = ' + copytext.Copy('data/%s.xlsx' % slug).json()
     return copy, 200, { 'content-type': 'application/javascript' }
 
+# Audio route to serve range headers for Safari.
+@static.route('/posts/<string:slug>/assets/<string:filename>.mp3')
+def audio(slug, filename):
+    from flask import Response, request
+
+    path = 'posts/%s/www/assets/%s.mp3' % (slug, filename)
+    with open(path) as f:
+        headers = Headers()
+        headers.add('Content-Disposition', 'attachment', filename=filename)
+        headers.add('Content-Transfer-Encoding','binary')
+
+        status = 200
+        size = os.path.getsize(path)
+        begin = 0
+        end = size - 1
+
+        if request.headers.has_key('Range'):
+            ranges = findall(r'\d+', request.headers['Range'])
+            begin = int(ranges[0])
+            if len(ranges) > 1:
+                end = int(ranges[1])
+
+            # status = 206
+            headers.add('Accept-Ranges', 'bytes')
+            headers.add('Content-Range', 'bytes %i-%i/%i' % (begin, end, end - begin) )
+
+        headers.add('Content-Length', str( (end - begin) + 1) )
+
+        response = Response(
+            file(path),
+            status = status,
+            mimetype = 'application/octet-stream',
+            headers = headers,
+            direct_passthrough = True
+        )
+
+        return response
+
 # server arbitrary static files on-demand
 def static_file(static_path, path):
     real_path = '%s/www/%s' % (static_path, path)
@@ -31,3 +72,4 @@ def static_file(static_path, path):
             return f.read(), 200, { 'Content-Type': guess_type(real_path)[0] }
     except IOError:
         abort(404)
+
