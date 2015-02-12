f306eb6bac95f4a7e018ee26a2c9ce6f056aff48
Claire O'Neill <coneill@npr.org>
2015-02-12 12:10:01 -0500

    tweet stylin'

--- posts/fugelsang/post_config.py
+++ posts/fugelsang/post_config.py
@@ -1,3 +1,3 @@
 COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=0AqjLQISCZzBkdFFtQlhnS2NiamJrcTczcWVxN21xRnc'
-DEPLOY_SLUG = 'fugelsang'
+DEPLOY_SLUG = 'lovestory'
 NUM_SLIDES_AFTER_CONTENT = 2
