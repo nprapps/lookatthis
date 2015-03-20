1a995c64e9eae666a64ff64cabeaf5532cf1e7c4
Claire O'Neill <coneill@npr.org>
2015-03-20 11:39:05 -0400

    astro PIL

---
+++ posts/astroreid-loves/post_config.py
@@ -0,0 +1,3 @@
+COPY_GOOGLE_DOC_URL = 'https://docs.google.com/spreadsheet/ccc?key=1Z67kNlLYqh8vdC5hpum3wzN8GIN1T8N1TkEl9M9r_xk&usp=drive_web#gid=9'
+DEPLOY_SLUG = 'astroreid-loves'
+NUM_SLIDES_AFTER_CONTENT = 2
