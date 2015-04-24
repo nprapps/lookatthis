5b019cef25bd045bcfa2badd70e699088163158d
Tyler Fisher <tyler@tylerjfisher.com>
2015-04-24 09:57:15 -0400

    set up proper URL parameters for GA campaign

--- posts/bus-station/post_config.py
+++ posts/bus-station/post_config.py
@@ -1,5 +1,3 @@
 COPY_GOOGLE_DOC_KEY = '0AsbgU6Ugtx7udHRpZllMemlHeWxsWEdNbXRuZW5aV0E'
 DEPLOY_SLUG = 'bus-station'
-NUM_SLIDES_AFTER_CONTENT = 2
-
-FB_SHARE_URL = 'http://apps.npr.org/lookatthis/bus-station'
+NUM_SLIDES_AFTER_CONTENT = 1
