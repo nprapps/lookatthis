11ea9f5646e5c29f234ba81fee4ee7cdf3516ec8
Tyler Fisher <tyler@tylerjfisher.com>
2015-05-21 10:40:42 -0400

    get share urls ready

--- posts/chris-clark/post_config.py
+++ posts/chris-clark/post_config.py
@@ -1,2 +1,3 @@
 COPY_GOOGLE_DOC_KEY = '0AsbgU6Ugtx7udG1aMjREakQ5eUVSb21vQXcxeFF2MWc'
 DEPLOY_SLUG = 'whales'
+EMAIL_SHARE_URL = 'http://apps.npr.org/lookatthis/posts/whales?utm_source=email&utm_medium=conclusion-share&utm_campaign=lookatthis'
