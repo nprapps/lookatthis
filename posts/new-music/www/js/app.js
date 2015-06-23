891b523b070014b521c7934f7cfb8f6433581590
Tyler Fisher <tyler@tylerjfisher.com>
2015-06-23 15:31:09 -0400

    fix up analytics for next-post

--- posts/new-music/www/js/app.js
+++ posts/new-music/www/js/app.js
@@ -24,6 +24,7 @@ var w;
 var h;
 var startTouch;
 var lastSlideExitEvent;
+var callToActionTest;
 var NO_AUDIO = (window.location.search.indexOf('noaudio') >= 0);
 var ASSETS_PATH = APP_CONFIG.DEPLOYMENT_TARGET ? APP_CONFIG.S3_BASE_URL + '/posts/' + APP_CONFIG.DEPLOY_SLUG + '/assets/' : 'http://assets.apps.npr.org.s3.amazonaws.com/lookatthis/' + APP_CONFIG.DEPLOY_SLUG + '/';
 
@@ -162,6 +163,9 @@ var onSlideChange = function(e, fromIndex, toIndex) {
     document.activeElement.blur();
     ANALYTICS.exitSlide(fromIndex.toString());
     ANALYTICS.trackEvent(lastSlideExitEvent, fromIndex.toString());
+    if (toIndex === $slides.length - 1) {
+        ANALYTICS.trackEvent('tests-run', callToActionTest);
+    }
 }
 
 var onStartCardButtonClick = function() {
