bdec86ad9a84c525b553081779796f9d63039150
Tyler Fisher <tyler@tylerjfisher.com>
2015-11-06 17:01:23 -0500

    cleanup graphics.js

--- posts/brazil/www/js/graphics.js
+++ posts/brazil/www/js/graphics.js
@@ -540,7 +540,6 @@ var GRAPHICS = (function() {
          * Apply adjustments to label positioning.
          */
         var positionLabel = function(adjustments, id, attribute) {
-            console.log(adjustments, id, attribute);
             if (adjustments[id]) {
                 if (adjustments[id][attribute]) {
                     return adjustments[id][attribute];
@@ -713,28 +712,6 @@ var GRAPHICS = (function() {
         }
 
         /*
-         * Render primary cities.
-         */
-
-        if (mapData['cities']) {
-            chartElement.append('g')
-                .attr('class', 'cities primary')
-                .selectAll('path')
-                    .data(mapData['cities']['features'])
-                .enter().append('path')
-                    .attr('d', path)
-                    .attr('class', function(d) {
-                        var c = 'place';
-
-                        c += ' ' + classify(d['properties']['city']);
-                        c += ' ' + classify(d['properties']['featurecla']);
-                        c += ' scalerank-' + d['properties']['scalerank'];
-
-                        return c;
-                    });
-        }
-
-        /*
          * Render country labels.
          */
 
@@ -796,81 +773,6 @@ var GRAPHICS = (function() {
         d3.select('.country-labels text.' + primaryCountryClass)
             .classed('label primary ' + primaryCountryClass, true);
 
-        /*
-         * Render city labels.
-         */
-        if (mapData['cities']) {
-            var layers = [
-                'city-labels shadow',
-                'city-labels',
-                'city-labels shadow primary',
-                'city-labels primary'
-            ];
-
-            layers.forEach(function(layer) {
-                var data = [];
-
-                if (layer == 'city-labels shadow' || layer == 'city-labels') {
-                    // data = mapData['neighbors']['features'];
-                } else {
-                    data = mapData['cities']['features'];
-                }
-
-                chartElement.append('g')
-                    .attr('class', layer)
-                    .selectAll('.label')
-                        .data(data)
-                    .enter().append('text')
-                        .attr('class', function(d) {
-                            var c = 'label';
-
-                            c += ' ' + classify(d['properties']['city']);
-                            c += ' ' + classify(d['properties']['featurecla']);
-                            c += ' scalerank-' + d['properties']['scalerank'];
-
-                            return c;
-                        })
-                        .attr('transform', function(d) {
-                            return 'translate(' + projection(d['geometry']['coordinates']) + ')';
-                        })
-                        .attr('style', function(d) {
-                            return 'text-anchor: ' + positionLabel(CITY_LABEL_ADJUSTMENTS, d['properties']['city'], 'text-anchor');
-                        })
-                        .attr('dx', function(d) {
-                            return positionLabel(CITY_LABEL_ADJUSTMENTS, d['properties']['city'], 'dx');
-                        })
-                        .attr('dy', function(d) {
-                            return positionLabel(CITY_LABEL_ADJUSTMENTS, d['properties']['city'], 'dy');
-                        })
-                        .text(function(d) {
-                            return d['properties']['city'];
-                        });
-            });
-
-            d3.selectAll('.shadow')
-                .attr('filter', 'url(#textshadow)');
-        }
-
-        // /*
-        //  * Render a scale bar.
-        //  */
-        // var scaleBarDistance = calculateOptimalScaleBarDistance(bbox, 10);
-        // var scaleBarStart = [10, mapHeight - 20];
-        // var scaleBarEnd = calculateScaleBarEndPoint(projection, scaleBarStart, scaleBarDistance);
-
-        // chartElement.append('g')
-        //     .attr('class', 'scale-bar')
-        //     .append('line')
-        //     .attr('x1', scaleBarStart[0])
-        //     .attr('y1', scaleBarStart[1])
-        //     .attr('x2', scaleBarEnd[0])
-        //     .attr('y2', scaleBarEnd[1]);
-
-        // d3.select('.scale-bar')
-        //     .append('text')
-        //     .attr('x', scaleBarEnd[0] + 5)
-        //     .attr('y', scaleBarEnd[1] + 3)
-        //     .text(scaleBarDistance + ' miles');
     }
 
     /*
